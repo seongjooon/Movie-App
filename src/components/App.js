bead5aaeac32b4271e4df77b1ffc1ad8affac75c
MrKamake <seongjooon@gmail.com>
2019-12-10 15:07:07 +0900

    add header that is main-logo

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.scss';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Main from './Main/Main';
+import Logo from '../image/movie-app-logo.png';
 
 class App extends Component {
   componentDidMount() {
@@ -9,16 +10,22 @@ class App extends Component {
   }
 
   render() {
-    const { movieList } = this.props;
+    const { movieList, genreList } = this.props;
 
     return (
       <div className="App">
+        <header>
+          <div className="main-logo">
+            <img src={Logo} alt="movie-app-logo" />
+            <div className='movie-app-text'>Movie App</div>
+          </div>
+        </header>
         <Switch>
           <Route exact path="/" render={() => <Redirect to="/main" />} />
           <Route
             exact
             path="/main"
-            render={() => <Main movieList={movieList} />}
+            render={() => <Main movieList={movieList} genreList={genreList} />}
           />
         </Switch>
       </div>
